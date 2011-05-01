55de04c648aaef5eb5fde6675e3013756da10a94
Ben Burnett <ben.burnett@gmail.com>
2011-05-01 00:38:48 -0600

    + Added more a more robust method for checking if a process is running + Also a method to determine if an executable is in the path

--- profile.d/00functions.sh
+++ profile.d/00functions.sh
@@ -7,23 +7,66 @@
 # purpose is rather an obscure trick.  Thus, to clarify our
 # intentions, we define an alias that hits more clearly at our
 # intended action.
-alias exists=which
+function exists {
+    which "$@"
+}
+
+# Since bash functions 'return' cannot return a value, we use echo in
+# place of a return value. This serves to emulate return values if we
+# are careful how we use the functions. An example of how we might use
+# this functions is:
+#
+# if [ $(os "Linux") ]; then
+#    # do something here
+# fi
+#
+# Where $(os "blah") is a function.  Using echo this way allows us to
+# treat function as if it were executables themselves.
+function return {
+    if [ ${1} -ne 0 ]; then
+	echo "false"
+    fi
+    exit
+}
+
+# From time to time we need to know is a particular OS is running.
+# The following will help us cleanly determine which OS is being used.
+function os {
+    if [ "`uname`" != "$@" ]; then
+	return 1
+    fi
+    return 0
+}
 
 # The Linux and Max OS X versions of the pidof tool are too different
-# to try and properly accomodate for.  Instead of clutering the code
-# below with some horid set of if statements, we abstract away the
-# differences here.
+# to try and properly accomodate for all the weird subtleties.
+# Instead of clutering the code below with some horid set of if
+# statements, we abstract away the differences here.
 function running {
-    local PID=0
+    local PID=
     if [ `uname` == "Darwin" ]; then
-	X=`pidof | grep ${USER} | grep balance | awk '{ printf $2 }'`
-	if [ "${X}x" != "x" ]; then
-	    PID=${X}
-	fi
+	PID=`pidof | grep ${USER} | grep balance | awk '{ printf $2 }'`	
     else
 	PID=`pidof "$@"`
     fi
-    echo ${PID}
+    if [ "${PID}x" == "x" ]; then
+	return 1
+    fi
+    return 0
+}
+
+# Just negate the result of the function we are given.  Note that we
+# make no attempt to determin if the function we are give actually
+# exists.  It would not be hard to add this functionality, but the
+# purpose of this is to be fast, not nessesarily safe.  We may revisit
+# this approach later.
+function not {
+    local NAME=$1
+    shift
+    if [ $(${NAME} "$@") ]; then
+	return 1
+    fi
+    return 0
 }
 
 ##
