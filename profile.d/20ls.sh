1a307fc4d7007cc0846c43b7a6be243e5ca4980f
Ben Burnett <ben.burnett@gmail.com>
2011-04-30 22:48:40 -0600

    + Fixed colour ls on Linux

--- profile.d/20ls.sh
+++ profile.d/20ls.sh
@@ -2,16 +2,23 @@
 # ls.sh
 ##
 
+# On Linux we need to tell 'ls' explicitly to use colours, even
+# through we have defined the environment correctly.
+LS='ls'
+if [ `uname` == "Linux" ]; then
+    COLOUR='ls --color'
+fi
+
 ##
 # ls aliases
 ##
 
-function ls { /bin/ls -Fh "$@" ; }
-function  l { /bin/ls -Fhl "$@" ; }
+function ls { ${LS} -Fh "$@" ; }
+function  l { ${LS} -Fhl "$@" ; }
 function ll { l "$@" ; }
-function la { /bin/ls -AFhl "$@" ; }
-function lr { /bin/ls -AFRhl "$@" ; }
-function l. { /bin/ls -Fdhl "$@".[a-zA-Z]* 2>/dev/null ; }
+function la { ${LS} -AFhl "$@" ; }
+function lr { ${LS} -AFRhl "$@" ; }
+function l. { ${LS} -Fdhl "$@".[a-zA-Z]* 2>/dev/null ; }
 
 ##
 # EOF
