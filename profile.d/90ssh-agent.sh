12a67f6cbbe78b70c916d1aa8179c3808d37b915
Ben Burnett <ben.burnett@gmail.com>
2011-05-04 23:41:17 -0600

    + Removed ssh-agent verboseness

--- profile.d/90ssh-agent.sh
+++ profile.d/90ssh-agent.sh
@@ -9,9 +9,7 @@
 SSH_ENV="${HOME}/.private/environment"
 
 function start_agent {
-    echo "Initialising new SSH agent... "
     /usr/bin/ssh-agent | sed 's/^echo/#echo/' >|"${SSH_ENV}"
-    echo "done."
     /bin/chmod 600 "${SSH_ENV}"
     . "${SSH_ENV}" >/dev/null
     /usr/bin/ssh-add    
