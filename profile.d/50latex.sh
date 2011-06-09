c06217f67d8606e0f39e4524054ad6c1c3912beb
Ben Burnett <ben.burnett@gmail.com>
2011-06-09 15:09:39 -0600

    + Switched from pdflatex to rubber

--- profile.d/50latex.sh
+++ profile.d/50latex.sh
@@ -8,19 +8,7 @@
 
 function makepdf { 
     NAME=${1%\.*}
-    TOOL=${2:-pdflatex}
-    if [[ -f ${NAME}.bib ]]; then
-	rm -f ${NAME}.bbl
-	${TOOL} ${NAME}
-	bibtex ${NAME}
-	${TOOL} ${NAME}
-    fi
-    ${TOOL} ${NAME}
-    if [ "${TOOL}" == "latex" ]; then
-	if ${TOOL} ${NAME}; then
-	    dvipdf ${NAME}
-	fi
-    fi
+    rubber --pdf ${NAME}
 }
 
 function openpdf {
