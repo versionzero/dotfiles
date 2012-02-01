8c4d27213f145a244ed16d64a1dda1161a232173
Ben Burnett <ben.burnett@gmail.com>
2012-01-31 20:16:05 -0700

    + Added some useful pdflatex command line options, like one that prints line numbers

--- profile.d/50latex.sh
+++ profile.d/50latex.sh
@@ -12,17 +12,21 @@ function cleanpdf {
     rm -f ${NAME}.pdf
 }
 
+function mypdflatex {
+    pdflatex --file-line-error --shell-escape --synctex=1 $*
+}
+
 function makepdf { 
     NAME=${1%\.*}
-    pdflatex ${NAME}.tex
+    mypdflatex ${NAME}.tex
     if [ -f ${NAME}-*.asy ]; then
 	asy ${NAME}-*.asy
     fi
     if [ -f ${NAME}.aux -a -f ${NAME}.bib ]; then
 	bibtex ${NAME}.aux
-	pdflatex ${NAME}.tex
+	mypdflatex ${NAME}.tex
     fi
-    pdflatex ${NAME}.tex
+    mypdflatex ${NAME}.tex
 }
 
 function openpdf {
