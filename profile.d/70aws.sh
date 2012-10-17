a92868f2b427c9067441c3ad734239a56cf0120a
Ben Burnett <ben.burnett@gmail.com>
2012-10-17 10:35:37 -0600

    Added support for the AWS tools

---
+++ profile.d/70aws.sh
@@ -0,0 +1,12 @@
+##
+# aws.sh - Amazon WebSevices Tools
+##
+
+export JAVA_HOME="$(/usr/libexec/java_home)"
+export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ec2/pk-*.pem | /usr/bin/head -1)"
+export EC2_CERT="$(/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)"
+export EC2_AMITOOL_HOME="/usr/local/Library/LinkedKegs/ec2-ami-tools/jars"
+
+##
+# EOF
+##
