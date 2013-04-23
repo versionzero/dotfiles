ddb4fa1d396c1caef02d918c366376c76fe54385
Ben Burnett <ben.burnett@gmail.com>
2013-04-23 16:38:10 -0600

    One machines with no ec2 access, don't try and load the env for it.

--- profile.d/70aws.sh
+++ profile.d/70aws.sh
@@ -3,9 +3,13 @@
 ##
 
 export JAVA_HOME="$(/usr/libexec/java_home)"
-export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ec2/pk-*.pem | /usr/bin/head -1)"
-export EC2_CERT="$(/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)"
-export EC2_AMITOOL_HOME="/usr/local/Library/LinkedKegs/ec2-ami-tools/jars"
+
+EC2_HOME="$HOME"/.ec2
+if [ -d "$EC2_HOME" ]; then
+    export EC2_PRIVATE_KEY="$(/bin/ls "$EC2_HOME"/pk-*.pem | /usr/bin/head -1)"
+    export EC2_CERT="$(/bin/ls "$EC2_HOME"/cert-*.pem | /usr/bin/head -1)"
+    export EC2_AMITOOL_HOME="/usr/local/Library/LinkedKegs/ec2-ami-tools/jars"
+fi
 
 ##
 # EOF
